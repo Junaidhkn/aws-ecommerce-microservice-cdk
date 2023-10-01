565ddf31847719387e95d01e0e979673a48ecac6
junaidhkn <junaidhkniazi@gmail.com>
2023-10-02 01:56:53 +0500

    lambda rest api

--- lib/ecommerce-microservice-cdk-stack.ts
+++ lib/ecommerce-microservice-cdk-stack.ts
@@ -1,4 +1,5 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, Billing, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import {
@@ -36,5 +37,21 @@ export class EcommerceMicroserviceCdkStack extends Stack {
 		});
 
 		productTable.grantReadWriteData(productFunction);
+
+		const apiGateWay = new LambdaRestApi(this, 'productAPI', {
+			restApiName: 'Product Service',
+			handler: productFunction,
+			proxy: false,
+		});
+
+		const product = apiGateWay.root.addResource('product'); // /product
+		product.addMethod('GET');
+		product.addMethod('POST');
+
+		const singleProduct = product.addResource('{id}'); // /product/{id}
+
+		singleProduct.addMethod('GET');
+		singleProduct.addMethod('PUT');
+		singleProduct.addMethod('DELETE');
 	}
 }
