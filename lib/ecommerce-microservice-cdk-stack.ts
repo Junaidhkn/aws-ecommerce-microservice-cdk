1ce90f71bb3f9cf382ce3062cbb3d2e28e6d798d
junaidhkn <junaidhkniazi@gmail.com>
2023-10-01 23:23:34 +0500

    lambda function

--- lib/ecommerce-microservice-cdk-stack.ts
+++ lib/ecommerce-microservice-cdk-stack.ts
@@ -1,6 +1,8 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { AttributeType, Billing, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
 export class EcommerceMicroserviceCdkStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,6 +13,12 @@ export class EcommerceMicroserviceCdkStack extends Stack {
 			tableName: 'product',
 			removalPolicy: RemovalPolicy.DESTROY,
 			billing: Billing.onDemand(),
+		});
+
+		const fn = new Function(this, 'MyFunction', {
+			runtime: Runtime.NODEJS_18_X,
+			handler: 'index.handler',
+			code: Code.fromAsset(join(__dirname, 'lambda-handler')),
 		});
 	}
 }
