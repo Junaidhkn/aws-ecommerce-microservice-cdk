4df382a9f2886f584dbec224e71e40dbe16e1c43
junaidhkn <junaidhkniazi@gmail.com>
2023-10-01 22:31:37 +0500

    dynamodb resourse

--- lib/ecommerce-microservice-cdk-stack.ts
+++ lib/ecommerce-microservice-cdk-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, Billing, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class EcommerceMicroserviceCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class EcommerceMicroserviceCdkStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcommerceMicroserviceCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		const productTable = new TableV2(this, 'product', {
+			partitionKey: { name: 'id', type: AttributeType.STRING },
+			tableName: 'product',
+			removalPolicy: RemovalPolicy.DESTROY,
+			billing: Billing.onDemand(),
+		});
+	}
 }
