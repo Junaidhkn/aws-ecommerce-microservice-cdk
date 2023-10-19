f57373c030891b2c4549fec5162c9547e3139cee
junaidhkn <junaidhkniazi@gmail.com>
2023-10-19 17:43:26 +0500

    microservice props

--- lib/ecommerce-microservice-cdk-stack.ts
+++ lib/ecommerce-microservice-cdk-stack.ts
@@ -1,10 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { EcomDatabase } from './database';
+import { EcomMicroservices } from './microservice';
 
 export class EcommerceMicroserviceCdkStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
+
+		const database = new EcomDatabase(this, 'ecommerceDatabase');
+		const microservice = new EcomMicroservices(this, 'ecommerceMicroservice', {
+			productTable: database.productTable,
+		});
 
 		const apiGateWay = new LambdaRestApi(this, 'productAPI', {
 			restApiName: 'Product Service',
