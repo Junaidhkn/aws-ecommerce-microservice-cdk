21fb581bf1df6f6a8d8e75729ebf6f7521819a59
junaidhkn <junaidhkniazi@gmail.com>
2023-10-21 18:04:07 +0500

    basket Database

--- lib/database.ts
+++ lib/database.ts
@@ -4,6 +4,7 @@ import { Construct } from 'constructs';
 
 export class EcomDatabase extends Construct {
 	public readonly productTable: TableV2;
+	public readonly basketTable: TableV2;
 
 	constructor(scope: Construct, id: string) {
 		super(scope, id);
@@ -16,5 +17,19 @@ export class EcomDatabase extends Construct {
 		});
 
 		this.productTable = productTable;
+
+		// Basket DynamoDb Table Creation
+		// Basket : PK: username --items (SET-MAP Object)
+		// Item1 : { quantity - color - price - productID - productName}
+		// Item2 : { quantity - color - price - productID - productName}
+
+		const basketTable = new TableV2(this, 'basket', {
+			partitionKey: { name: 'userName', type: AttributeType.STRING },
+			tableName: 'basket',
+			removalPolicy: RemovalPolicy.DESTROY,
+			billing: Billing.onDemand(),
+		});
+
+		this.basketTable = basketTable;
 	}
 }
