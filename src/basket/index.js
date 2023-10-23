d20b11cd888bb28abb7218032dec44d4240dc228
junaidhkn <junaidhkniazi@gmail.com>
2023-10-24 02:23:25 +0500

    baskets methods

--- src/basket/index.js
+++ src/basket/index.js
@@ -1,7 +1,6 @@
-import { DeleteItemCommand, GetItemCommand, PutItemCommand, ScanCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { DeleteItemCommand, GetItemCommand, PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { ddbClient } from "./DynamodbClient.js";
-import { v4 as uuid } from 'uuid';
 
 
 exports.handler = async function ( event ) {
@@ -12,25 +11,24 @@ exports.handler = async function ( event ) {
       let body;
       switch ( event.httpMethod ) {
          case "GET":
-            if ( event.queryStringParameters != null ) {
-               body = await getBasket( event.pathParameters.userName );
+            if ( event.pathParameters != null ) {
+               body = await getBasket( event.pathParameters.userName ); // GET /basket/{userName}
             } else {
-               body = await getAllBaskets();
+               body = await getAllBaskets(); // GET /basket
             }
             break;
          case "POST":
-            if ( event.path === '/basket/checkout' ) {
-               body = await checkoutBasket( event )
+            if ( event.path == "/basket/checkout" ) {
+               body = await checkoutBasket( event ); // POST /basket/checkout
             } else {
-               const basket = JSON.parse( event.body );
-               body = await createBasket( basket );
+               body = await createBasket( event ); // POST /basket
             }
             break;
          case "DELETE":
-            body = await deleteBasket( event.pathParameters.userName );
+            body = await deleteBasket( event.pathParameters.userName ); // DELETE /basket/{userName}
             break;
          default:
-            throw new Error( `Unsupported method "${event.httpMethod}"` );
+            throw new Error( `Unsupported route: "${event.httpMethod}"` );
       }
 
       return {
@@ -56,84 +54,85 @@ exports.handler = async function ( event ) {
 }
 
 
-const getBasket = async ( BasketId ) => {
-   console.log( "getBasket", BasketId )
+const getBasket = async ( userName ) => {
+   console.log( "getBasket" );
    try {
       const params = {
          TableName: process.env.DYNAMODB_TABLE_NAME,
-         Key: marshall( {
-            id: BasketId
-         } ),
-      }
-      const { Item } = await ddbClient.send( new GetItemCommand( params ) )
-      console.log( "Success", Item )
-      return ( Item ) ? unmarshall( Item ) : {}
-   } catch ( error ) {
-      console.log( error )
-      throw error
-   }
+         Key: marshall( { userName: userName } )
+      };
 
+      const { Item } = await ddbClient.send( new GetItemCommand( params ) );
+
+      console.log( Item );
+      return ( Item ) ? unmarshall( Item ) : {};
+
+   } catch ( e ) {
+      console.error( e );
+      throw e;
+   }
 }
 
 const getAllBaskets = async () => {
-   console.log( 'Get all Baskets' )
+   console.log( "getAllBaskets" );
    try {
       const params = {
-         TableName: process.env.DYNAMODB_TABLE_NAME,
+         TableName: process.env.DYNAMODB_TABLE_NAME
       };
+
       const { Items } = await ddbClient.send( new ScanCommand( params ) );
-      console.log( Items )
+
+      console.log( Items );
       return ( Items ) ? Items.map( ( item ) => unmarshall( item ) ) : {};
-   } catch ( error ) {
-      console.log( error )
-      throw error
+
+   } catch ( e ) {
+      console.error( e );
+      throw e;
    }
 }
-// If there is an error try parsing the event body in the try catch block rather than the switch case
-const createBasket = async ( Basket ) => {
-   console.log( 'Create Basket', Basket )
+const createBasket = async ( event ) => {
+   console.log( `createBasket function. event : "${event}"` );
    try {
-      const BasketId = uuid();
-      Basket.id = BasketId;
+      const requestBody = JSON.parse( event.body );
       const params = {
          TableName: process.env.DYNAMODB_TABLE_NAME,
-         Item: marshall( Basket )
-      }
-      const response = await ddbClient.send( new PutItemCommand( params ) )
-      console.log( "Success", response.Item )
-      return unmarshall( response.Item )
-   } catch ( error ) {
-      console.log( error )
-      throw error
+         Item: marshall( requestBody || {} )
+      };
+
+      const createResult = await ddbClient.send( new PutItemCommand( params ) );
+      console.log( createResult );
+      return createResult;
+
+   } catch ( e ) {
+      console.error( e );
+      throw e;
    }
 }
 
-const deleteBasket = async ( BasketId ) => {
-   console.log( 'Delete Basket', BasketId )
+const deleteBasket = async ( userName ) => {
+   console.log( `deleteBasket function. userName : "${userName}"` );
    try {
       const params = {
          TableName: process.env.DYNAMODB_TABLE_NAME,
-         Key: marshall( {
-            id: BasketId
-         } ),
-      }
-      const response = await ddbClient.send( new DeleteItemCommand( params ) )
-      console.log( "Success", response )
-      return unmarshall( response )
-   } catch ( error ) {
-      console.log( error )
-      throw error
+         Key: marshall( { userName: userName } ),
+      };
+
+      const deleteResult = await ddbClient.send( new DeleteItemCommand( params ) );
+      console.log( deleteResult );
+      return deleteResult;
+
+   } catch ( e ) {
+      console.error( e );
+      throw e;
    }
 }
-
 
 const checkoutBasket = async ( event ) => {
-   console.log( 'checkoutBasket', event )
-   try {
+   console.log( "checkoutBasket" );
+   // implement function   
 
-   } catch ( error ) {
-      console.log( error )
-      throw error
-   }
+   // publish an event to eventbridge - this will subscribe by order microservice 
+   // and start ordering process.
+
 }
 
